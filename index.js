6a0dc3710224268cc6bfb524d379b5e6e0129ed1
Oleg <50801614+jsonic1337@users.noreply.github.com>
2020-12-25 13:30:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,61 @@
 var EmojiBot = require('./modules/EmojiBot.js');
 var GlitchMsgBot = require('./modules/GlitchMsgBot.js');
+var GachiBot = require('./modules/GachiBot.js');
+var VKmeFunc = require('./modules/VKmeFunc.js');
+var config = require('./config.js');
+
+
+
+
 const {
 	VK
 } = require('vk-io');
 const vk = new VK({
-	token: "TOKEN" // Сюда нужно вставить токен с правами на сообщения
+	token: config.TOKEN
 });
 var commands = {
-	'.эм': 'emoji',
-	'.влр': 'glitch'
+	'emoji': config.emoji,
+	'glitch': config.glitch,
+	'gachi': config.gachi,
+	'vkme': config.vkme
 }
 var m_id = 0;
 var set = false;
 vk.updates.start();
 vk.updates.on('message', (context, next) => {
 	if (context.hasText == false || context.isOutbox == false || context.hasAttachments() == true) return
-	if (Object.keys(commands).includes(context.text)) {
+
+
+	if (Object.values(commands).includes(context.text)) {
 		m_id = context.id;
-		set = commands[context.text]
+		set = getKeyByValue(commands, context.text)
 		context.deleteMessage({
 			message_ids: context.id,
 			delete_for_all: 1
 		})
-	} else if (context.text == '.выкл') {
+	} else if (context.text == config.off) {
 		set = false
 		context.deleteMessage({
 			message_ids: context.id,
 			delete_for_all: 1
 		})
+
+
 	} else if (m_id != context.id && set == 'emoji') {
 		EmojiBot(context);
 		m_id = context.id;
 	} else if (m_id != context.id && set == 'glitch') {
 		GlitchMsgBot(context);
 		m_id = context.id;
+	} else if (m_id != context.id && set == 'gachi') {
+		GachiBot(context);
+		m_id = context.id;
+	} else if (m_id != context.id && set == 'vkme') {
+		VKmeFunc(context, config.vkme_command, config.regex, config.TOKEN);
+		m_id = context.id;
 	}
 })
+const getKeyByValue = (obj, value) => 
+        Object.keys(obj).find(key => obj[key] === value);
+
+
