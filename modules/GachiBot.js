e8eb6412e8f88bdd6ef9b3a6a1d85a36181f8a7b
Oleg <50801614+jsonic1337@users.noreply.github.com>
2020-12-25 13:32:19 +0300

    Create GachiBot.js

---
+++ modules/GachiBot.js
@@ -0,0 +1,19 @@
+gachiMassive = ['♂THREE HUNDRED BUCKS♂', '♂dungeon Master♂', '♂ass we can!♂', '♂DEEP DARK FANTASY♂', '♂swallow♂', '♂my cum♂', '♂suck some dick♂', '♂fucking slaves♂', '♂in my ass♂', '♂fuck you♂', '♂finger♂', '♂FUCK♂', '♂stick your finger in my ass♂', '♂boy next door♂', '♂fisting♂', '♂leatherclubs♂', '♂fat cock♂', '♂semen♂', '♂in my ass♂', '♂cum back♂', '♂fisting Master♂', '♂glove in my ass♂', '♂bondage Master♂', '♂cum♂', '♂fisting♂', '♂anal♂', '♂gay boy♂', '♂semen♂', '♂wee-wee♂', '♂its so fucking deep♂']
+
+var GachiBot = function(context) {
+	var dataText = context.text.split(' ')
+	for (var itemIndex = 1; itemIndex < dataText.length; itemIndex += 2) {
+		if (getRandom(3) == 2) {
+        dataText.splice(itemIndex, 0, gachiMassive[getRandom(gachiMassive.length)]);
+        }
+	}
+if (context.text != dataText.join(" ")){
+	context.editMessage({
+		message: `${dataText.join(" ")}`
+	}) }
+}
+
+function getRandom(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
+module.exports = GachiBot;
