1351352f38615c4a82cc098d1e3f6dc73560b776
Oleg <50801614+jsonic1337@users.noreply.github.com>
2021-01-07 01:13:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ var GachiBot = require('./modules/GachiBot.js');
 var VKmeFunc = require('./modules/VKmeFunc.js');
 var TickerBot = require('./modules/TickerBot.js');
 var config = require('./config.js');
-
-
-
-
 const {
 	VK
 } = require('vk-io');
@@ -21,46 +17,42 @@ var commands = {
 	'vkme': config.vkme,
 	'ticker': config.ticker
 }
-var m_id = 0;
 var set = false;
 vk.updates.start();
 vk.updates.on('message', (context, next) => {
-	if (context.hasText == false || context.isOutbox == false || context.hasAttachments() == true) return
-
-
+	if (context.hasText == false || context.isOutbox == false || context.hasAttachments() == true || context.subTypes[0] != 'message_new') return
+	m_id = false
 	if (Object.values(commands).includes(context.text)) {
-		m_id = context.id;
 		set = getKeyByValue(commands, context.text)
 		context.deleteMessage({
 			message_ids: context.id,
 			delete_for_all: 1
 		})
+		m_id = true
 	} else if (context.text == config.off) {
 		set = false
 		context.deleteMessage({
 			message_ids: context.id,
 			delete_for_all: 1
 		})
-
-
-	} else if (m_id != context.id && set == 'emoji') {
-		EmojiBot(context);
-		m_id = context.id;
-	} else if (m_id != context.id && set == 'glitch') {
-		GlitchMsgBot(context);
-		m_id = context.id;
-	} else if (m_id != context.id && set == 'gachi') {
-		GachiBot(context);
-		m_id = context.id;
-	} else if (m_id != context.id && set == 'vkme') {
-		VKmeFunc(context, config.vkme_command, config.regex, config.offline, config.online, config.TOKEN);
-		m_id = context.id;
-	} else if (m_id != context.id && set == 'ticker') {
-		TickerBot(context, config.pref_ticker);
-		m_id = context.id;
+	}
+	if (m_id == true) return
+	switch (set) {
+		case "emoji":
+			EmojiBot(context);
+			break;
+		case "glitch":
+			GlitchMsgBot(context);
+			break;
+		case "gachi":
+			GachiBot(context);
+			break;
+		case "vkme":
+			VKmeFunc(context, config.vkme_command, config.regex, config.offline, config.online, config.TOKEN);
+			break;
+		case "ticker":
+			TickerBot(context, config.pref_ticker);
+			break;
 	}
 })
-const getKeyByValue = (obj, value) => 
-        Object.keys(obj).find(key => obj[key] === value);
-
-
+const getKeyByValue = (obj, value) => Object.keys(obj).find(key => obj[key] === value);
